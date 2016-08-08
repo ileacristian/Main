5202dc0047aa1e187ffc4acf331b2c37af8a0fce
Cristian Ilea <cristianilea@192-168-0-112.rdsnet.ro>
2016-08-08 20:06:03 +0300

    initial commit

---
+++ Main/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Main
+//
+//  Created by Cristian Ilea on 8/8/16.
+//  Copyright © 2016 Cristian Ilea. All rights reserved.
+//
+
+import UIKit
+import FooBar
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(fooBar())
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
